2aecbfc2d24357f90fe616ce2dbea55ec3c16ebd
Your Name <you@example.com>
2024-04-20 20:56:57 +0900

    norm ok

--- src/destory.c
+++ src/destory.c
@@ -1,20 +1,20 @@
 #include "philo.h"
 
-void fork_destory(pthread_mutex_t *forks,int i)
+void	fork_destory(pthread_mutex_t *forks, int i)
 {
-			while(--i)
-			{
-				pthread_mutex_destroy(&forks[i]);
-			}
-			free(forks);
+	while (--i)
+	{
+		pthread_mutex_destroy(&forks[i]);
+	}
+	free(forks);
 }
 
 /* void philo_destory(t_philo *philos) */
-void philo_destory(t_philo *philos,int i)
+void	philo_destory(t_philo *philos, int i)
 {
-			while(--i)
-			{
-				pthread_mutex_destroy(&philos[i].mutex_philo);
-			}
-			free(philos);
+	while (--i)
+	{
+		pthread_mutex_destroy(&philos[i].mutex_philo);
+	}
+	free(philos);
 }
