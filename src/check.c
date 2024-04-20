c5c26abdd5ff756179422ab4db4e960d0293ca47
Your Name <you@example.com>
2024-04-20 20:56:07 +0900

    norm ok

--- src/check.c
+++ src/check.c
@@ -1,91 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyamauch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 20:55:31 by tyamauch          #+#    #+#             */
+/*   Updated: 2024/04/20 20:55:48 by tyamauch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-/* number; */
-/* time_die; */
-/* time_eat; */
-/* time_sleep; */
-/* must_eat; */
-
-/* number > 0 */
-
-bool input_check(int argc ,char **argv)
+bool	input_check(int argc, char **argv)
 {
-	const char *str;
+	const char	*str;
 
-	if(argc < 5 || argc > 6 || *argv[1] == '0') 		
+	if (argc < 5 || argc > 6 || *argv[1] == '0')
 	{
-				ft_put_error(ARGS_ERROR);
-				return(false);
+		ft_put_error(ARGS_ERROR);
+		return (false);
 	}
-	while(*++argv)
+	while (*++argv)
 	{
-			str = *argv;
-			while(*str)
+		str = *argv;
+		while (*str)
+		{
+			if (!ft_isdigit(*str))
 			{
-					if(!ft_isdigit(*str))
-					{
-						ft_put_error(ARGS_ERROR);
-						return(false);
-					}
-					str++;
+				ft_put_error(ARGS_ERROR);
+				return (false);
 			}
+			str++;
+		}
 	}
-	return(true);
+	return (true);
 }
 
-static bool is_hunger(t_philo *philos)
+static bool	is_hunger(t_philo *philos)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(i < philos->info->number)
+	while (i < philos->info->number)
 	{
 		pthread_mutex_lock(&philos[i].mutex_philo);
-		if(get_now_time() - philos[i].active_time > philos->info->time_die) 
+		if (get_now_time() - philos[i].active_time > philos->info->time_die)
 		{
 			pthread_mutex_unlock(&philos[i].mutex_philo);
-			put_log(&philos[i],DIED);
-			return(true);
+			put_log(&philos[i], DIED);
+			return (true);
 		}
 		pthread_mutex_unlock(&philos[i].mutex_philo);
 		i++;
 	}
-	return(false);
+	return (false);
 }
 
-static bool is_satiety(t_philo *philos)
+static bool	is_satiety(t_philo *philos)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(i < philos->info->number)
+	while (i < philos->info->number)
 	{
 		pthread_mutex_lock(&philos[i].mutex_philo);
-		if(philos[i].eat_count < philos->info->must_eat)
+		if (philos[i].eat_count < philos->info->must_eat)
 		{
 			pthread_mutex_unlock(&philos[i].mutex_philo);
-				return(false);
+			return (false);
 		}
 		pthread_mutex_unlock(&philos[i].mutex_philo);
 		i++;
 	}
-	return(true);
+	return (true);
 }
 
-bool finish_check(t_philo *philos)
+bool	finish_check(t_philo *philos)
 {
-	if(philos->info->must_eat == -1 )
+	if (philos->info->must_eat == -1)
 	{
-		if(is_hunger(philos))
-			return(true);
+		if (is_hunger(philos))
+			return (true);
 		else
-			return(false);
+			return (false);
 	}
 	else
 	{
-		if(is_hunger(philos) || is_satiety(philos))
-			return(true);
+		if (is_hunger(philos) || is_satiety(philos))
+			return (true);
 		else
-			return(false);
+			return (false);
 	}
 }
