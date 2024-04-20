c79e7ac847545b3f7a2d6aea8cdd9bef0f4f5cc3
Your Name <you@example.com>
2024-04-20 22:12:20 +0900

    norm ok

--- src/routine.c
+++ src/routine.c
@@ -1,61 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyamauch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 22:10:39 by tyamauch          #+#    #+#             */
+/*   Updated: 2024/04/20 22:10:41 by tyamauch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-static void take_fork(t_philo *philo)
+static void	take_fork(t_philo *philo)
 {
-	if(philo->id % 2 == 0)
+	if (philo->id % 2 == 0)
 		pthread_mutex_lock(philo->r_fork);
 	else
 		pthread_mutex_lock(philo->l_fork);
-	put_log(philo,TAKE_FORK);
-	if(philo->r_fork == philo->l_fork)
+	put_log(philo, TAKE_FORK);
+	if (philo->r_fork == philo->l_fork)
 	{
 		actual_usleep(philo->info->time_die * 1.1);
 		pthread_mutex_unlock(philo->r_fork);
 	}
-	if(philo->id % 2 == 0)
+	if (philo->id % 2 == 0)
 		pthread_mutex_lock(philo->l_fork);
 	else
 		pthread_mutex_lock(philo->r_fork);
-	put_log(philo,TAKE_FORK);
+	put_log(philo, TAKE_FORK);
 }
 
-static void eating(t_philo *philo)
+static void	eating(t_philo *philo)
 {
-	put_log(philo,EATING);
-	actual_usleep(philo->info->time_eat );
+	put_log(philo, EATING);
+	actual_usleep(philo->info->time_eat);
 	pthread_mutex_lock(&philo->mutex_philo);
-	philo->active_time = get_now_time(); 
+	philo->active_time = get_now_time();
 	philo->eat_count++;
 	pthread_mutex_unlock(&philo->mutex_philo);
 	pthread_mutex_unlock(philo->l_fork);
 	pthread_mutex_unlock(philo->r_fork);
 }
 
-static void sleeping(t_philo *philo)
+static void	sleeping(t_philo *philo)
 {
-	put_log(philo,SLEEPING);
-	actual_usleep(philo->info->time_sleep );
+	put_log(philo, SLEEPING);
+	actual_usleep(philo->info->time_sleep);
 }
 
-static void thinking(t_philo *philo)
+static void	thinking(t_philo *philo)
 {
-	put_log(philo,THINKING);
+	put_log(philo, THINKING);
 }
 
-void *routine(void *arg)
+void	*routine(void *arg)
 {
-	t_philo *philos;
-	
+	t_philo	*philos;
+
 	philos = (t_philo *)arg;
-	if(philos->id %2 == 0)
+	if (philos->id % 2 == 0)
 		actual_usleep(1);
-	while(true)
+	while (true)
 	{
 		pthread_mutex_lock(&philos->mutex_philo);
-		if(philos->is_dead)
+		if (philos->is_dead)
 		{
 			pthread_mutex_unlock(&philos->mutex_philo);
-			break;
+			break ;
 		}
 		pthread_mutex_unlock(&philos->mutex_philo);
 		take_fork(philos);
@@ -63,6 +75,5 @@ void *routine(void *arg)
 		sleeping(philos);
 		thinking(philos);
 	}
-	return(NULL);
+	return (NULL);
 }
-
