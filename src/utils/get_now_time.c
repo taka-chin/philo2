34f49a8fdb71dda76c6c9f7a380c07766fea036f
Your Name <you@example.com>
2024-04-21 17:17:37 +0900

    計算方法変えた

--- src/utils/get_now_time.c
+++ src/utils/get_now_time.c
@@ -6,7 +6,7 @@
 /*   By: tyamauch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 22:16:08 by tyamauch          #+#    #+#             */
-/*   Updated: 2024/04/20 22:16:37 by tyamauch         ###   ########.fr       */
+/*   Updated: 2024/04/21 17:14:18 by tyamauch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ long int	get_now_time(void)
 	long int		now_time;
 
 	gettimeofday(&tp, NULL);
-	now_time = tp.tv_sec * 1000000;
-	now_time += tp.tv_usec ;
-	now_time = now_time / 1000;
+	now_time = tp.tv_sec * 1000;
+	now_time += tp.tv_usec /1000;
 	return (now_time);
 }
