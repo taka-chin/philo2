0720a3dd4e4658fa3b6157ce789366b35802accf
Your Name <you@example.com>
2024-04-21 17:17:00 +0900

    計算方法を修正

--- src/utils/create_log_time.c
+++ src/utils/create_log_time.c
@@ -6,7 +6,7 @@
 /*   By: tyamauch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 22:15:40 by tyamauch          #+#    #+#             */
-/*   Updated: 2024/04/20 22:15:42 by tyamauch         ###   ########.fr       */
+/*   Updated: 2024/04/21 17:16:05 by tyamauch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ long int	create_log_time(t_philo *philo)
 	long int		log_time;
 
 	gettimeofday(&tp, NULL);
-	log_time = (tp.tv_sec - philo->start_time.tv_sec) * 1000000;
-	log_time += (tp.tv_usec - philo->start_time.tv_usec);
-	log_time /= 1000;
+	log_time = (tp.tv_sec - philo->start_time.tv_sec) *1000;
+	log_time += (tp.tv_usec - philo->start_time.tv_usec)/1000;
 	return (log_time);
 }
