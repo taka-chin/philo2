9f4a118f5787309d85973a1c51134e64c18297ac
Your Name <you@example.com>
2024-04-22 17:50:08 +0900

    fix

--- src/utils/actual_usleep.c
+++ src/utils/actual_usleep.c
@@ -6,17 +6,29 @@
 /*   By: tyamauch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 22:13:57 by tyamauch          #+#    #+#             */
-/*   Updated: 2024/04/20 22:13:59 by tyamauch         ###   ########.fr       */
+/*   Updated: 2024/04/22 17:43:41 by tyamauch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static long int	diff_time(struct timeval now)
+{
+	struct timeval	tp;
+	long int		log_stime;
+	long int		log_utime;
+
+	gettimeofday(&tp, NULL);
+	log_stime = (tp.tv_sec - now.tv_sec) * 1000000;
+	log_utime = (tp.tv_usec - now.tv_usec);
+	return ((log_stime + log_utime) / 1000);
+}
+
 void	actual_usleep(long int sleep_time)
 {
-	long int	now;
+	struct timeval	now;
 
-	now = get_now_time();
-	while (get_now_time() - now < sleep_time)
+	gettimeofday(&now, NULL);
+	while (diff_time(now) < sleep_time)
 		usleep(100);
 }
